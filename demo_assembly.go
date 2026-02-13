60f7457ca526f0e4197e5f757914e02d966fde6f
Andre Vanha <andre@vanhas.com>
2026-02-13 16:00:15 -0700

    Initial commit

---
+++ demo_assembly.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/avanha/pmaas-core"
+	"github.com/avanha/pmaas-core/config"
+	basicwebui "github.com/avanha/pmaas-plugin-basicwebui"
+)
+
+func main() {
+	fmt.Printf("Starting Demo PMAAS assembly\n")
+	conf := config.NewConfig()
+	conf.HttpPort = 8090
+
+	addSimpleWebUI(conf)
+
+	var pmaas = core.NewPMAAS(conf)
+	err := pmaas.Run()
+
+	if err != nil {
+		fmt.Printf("pmaas.Run completed with error: %s\n", err)
+	}
+
+	fmt.Printf("Demo PMAAS assembly terminated\n")
+}
+
+func addSimpleWebUI(serverConfig *config.Config) {
+	conf := basicwebui.NewWebRenderPluginConfig()
+	serverConfig.AddPlugin(basicwebui.NewWebRenderPlugin(conf), config.PluginConfig{
+		//ContentPathOverride: localProjectRoot + "/plugins/webrender/content",
+	})
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/avanha/pmaas-assembly-demo
+
+go 1.25.7
+
+require (
+	github.com/avanha/pmaas-core v0.0.1 // indirect
+	github.com/avanha/pmaas-plugin-basicwebui v0.0.0 // indirect
+	github.com/avanha/pmaas-spi v0.0.0 // indirect
+)
